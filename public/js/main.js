cbf5bd15c20fda8ad7e018f348d750caab9096cf
Dmitry Shved <dshved@hotmail.com>
2016-08-25 17:04:57 +0300

    Added header style

--- public/js/main.js
+++ public/js/main.js
@@ -2,18 +2,21 @@ $('#search_vehues').click(function(e) {
   e.preventDefault();
   $('#list').empty();
   $.ajax({
-    url: '/api/venues/search',
-    data: {ll: $('#ll').val()},
-  })
-  .done(function(data) {
-    var venues = data['response']['venues'];
+      url: '/api/venues/search',
+      data: { ll: $('#ll').val() },
+    })
+    .done(function(data) {
+      var venues = data['response']['venues'];
 
-    for (var i = 0; i < venues.length; i++) {
-      $('#list').append('<li>'+venues[i]['name']+'</li>');    
-    };
-  })
-  .fail(function(data) {
-    console.log(data);
-  });
-  
+      for (var i = 0; i < venues.length; i++) {
+        $('#list').append('<li>' + venues[i]['name'] + '</li>');
+      };
+    })
+    .fail(function(data) {
+      console.log(data);
+    });
+
 });
+
+
+
