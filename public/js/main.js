6d9b18ac8c4b9da079478cbcad8b71c9201c523e
Dmitry Shved <dshved@hotmail.com>
2016-08-26 11:28:14 +0300

    Changed google maps style

--- public/js/main.js
+++ public/js/main.js
@@ -37,6 +37,39 @@ function initMap() {
     overviewMapControlOptions: {
       opened: false,
     },
+    styles: [{
+      "featureType": "poi",
+      "elementType": "all",
+      "stylers": [{
+        "visibility": "off"
+      }]
+    }, {
+      "featureType": "road",
+      "elementType": "all",
+      "stylers": [{
+        "saturation": -100
+      }, {
+        "lightness": 45
+      }]
+    }, {
+      "featureType": "road.highway",
+      "elementType": "all",
+      "stylers": [{
+        "visibility": "simplified"
+      }]
+    }, {
+      "featureType": "road.arterial",
+      "elementType": "labels.icon",
+      "stylers": [{
+        "visibility": "off"
+      }]
+    }, {
+      "featureType": "transit",
+      "elementType": "all",
+      "stylers": [{
+        "visibility": "off"
+      }]
+    }],
   });
 
   map.addListener('click', function(e) {
