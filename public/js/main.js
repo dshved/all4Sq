78200497ee803b138094a50f43a948f78316bcba
Dmitry Shved <dshved@hotmail.com>
2016-08-26 09:56:10 +0300

    Added google maps

--- public/js/main.js
+++ public/js/main.js
@@ -1,9 +1,11 @@
-$('#search_vehues').click(function(e) {
-  e.preventDefault();
+var map;
+var markers = [];
+
+function showVenues(lat, lng) {
   $('#list').empty();
   $.ajax({
       url: '/api/venues/search',
-      data: { ll: $('#ll').val() },
+      data: { ll: lat + ',' + lng },
     })
     .done(function(data) {
       var venues = data['response']['venues'];
@@ -15,8 +17,56 @@ $('#search_vehues').click(function(e) {
     .fail(function(data) {
       console.log(data);
     });
+}
 
-});
+function initMap() {
+  var haightAshbury = { lat: 59.937475, lng: 30.352243 };
+
+  map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 12,
+    center: haightAshbury,
+    mapTypeId: google.maps.MapTypeId.ROADMAP,
+    mapTypeControl: false,
+    scaleControl: false,
+    scrollwheel: true,
+    panControl: false,
+    streetViewControl: false,
+    draggable: true,
+    overviewMapControl: false,
+    overviewMapControlOptions: {
+      opened: false,
+    },
+  });
+
+  map.addListener('click', function(e) {
+    addMarker(e.latLng);
+    showVenues(e.latLng.lat(), e.latLng.lng());
+  });
+}
+
+function addMarker(location) {
+  deleteMarkers();
+  var marker = new google.maps.Marker({
+    position: location,
+    map: map
+  });
+  markers.push(marker);
+}
 
 
+function setMapOnAll(map) {
+  for (var i = 0; i < markers.length; i++) {
+    markers[i].setMap(map);
+  }
+}
 
+function clearMarkers() {
+  setMapOnAll(null);
+}
+
+function deleteMarkers() {
+  clearMarkers();
+  markers = [];
+}
+
+initMap();
